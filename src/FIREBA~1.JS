1d966375eb9d1a08102cf0b64a92cfba71a1fd31
FromICN <sho90427@gmail.com>
2026-08-03 07:00:59 +0900

    Add files via upload

---
+++ src/FIREBA~1.JS
@@ -0,0 +1,9 @@
+/* Firebase 콘솔 웹 앱 설정 (lifelog-f5275) */
+export const firebaseConfig = {
+  apiKey: "AIzaSyDQ3DpiYTYtxInHnBwdKMmTB-e8gjr93vU",
+  authDomain: "lifelog-f5275.firebaseapp.com",
+  projectId: "lifelog-f5275",
+  storageBucket: "lifelog-f5275.firebasestorage.app",
+  messagingSenderId: "703511616322",
+  appId: "1:703511616322:web:a7cd349dc601c9a98206db",
+};
